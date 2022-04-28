5ca97625664150fbc5e8053a9b0ee46b116d1351
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-04-28 14:02:38 -0400

    trying to fix image css

--- frontend/components/product/product_index_item.jsx
+++ frontend/components/product/product_index_item.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const ProductIndexItem = ({product}) => (
-    <div className="product-index-item">
-            <Link className="product-index-item-img" to={`/products/${product.id}`}>
-                <p>Image Placeholder</p>
-            </Link>
-            <Link className="product-index-item-name" to={`/products/${product.id}`}>
-                <p>{product.name}</p>
-            </Link>
-            <Link className="product-index-item-price" to={`/products/${product.id}`}>
-                <p>${product.price}</p>
-            </Link>
-        <p>Get it in 1 week!</p>
-        <br />
-        <p>FREE shipping by Amajon</p>
-    </div>
-)
+
+const ProductIndexItem = ({product}) => {
+    var imageurl;
+    if (product.id % 3 === 0){
+        imageurl = window.longproductURL;
+    }else if (product.id % 3 === 1){
+        imageurl = window.squareproductURL;
+    }else{
+        imageurl = window.flatproductURL;
+    }
+    return (
+        <div className="product-index-item">
+            <div className="index-img-container">
+                <Link to={`/products/${product.id}`}>
+                    <img className="product-index-item-img" src={imageurl} />
+                </Link>
+            </div>
+            <div className="product-index-item-name-container">
+                <Link className="product-index-item-name" to={`/products/${product.id}`}>
+                    <span>{product.name}</span>
+                </Link>
+            </div>
+            <div className="product-index-item-price-container">
+                <Link className="product-index-item-price" to={`/products/${product.id}`}>
+                    <span>${product.price}</span>
+                </Link>
+            </div>
+            <div className="product-index-item-shipping-containner">
+                <span>Get it in 1 week!</span>
+                <span>FREE shipping by Amajon</span>
+            </div>
+        </div>
+    )
+}
 
 export default ProductIndexItem;
 

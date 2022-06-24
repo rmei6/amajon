3d8b4eda9a902b0cb8387adebd758c1ffb5c7208
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-06-23 21:50:38 -0400

    set up database for reviews

--- db/migrate/20220617032145_add_review_column.rb
+++ db/migrate/20220617032145_add_review_column.rb
@@ -1,5 +1,5 @@
 class AddReviewColumn < ActiveRecord::Migration[5.2]
   def change
-    add_column :products, :review, :integer, null:false
+    add_column :products, :review, :float, null:false
   end
 end

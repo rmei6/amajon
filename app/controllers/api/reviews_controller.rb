65e6de7f38ca86db3c392aaeb49ec9fc4e9144a6
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-07-08 22:39:54 -0400

    did review controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,21 @@
 class Api::ReviewsController < ApplicationController
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def review_params
+    
+  end
 end
